ba9136925802d59a63fbc525f9c09a8181b8c924
Matthew Fogarty <mattempyre@gmail.com>
2023-08-02 18:49:56 +0200

    Added getCurrentUser & getSession actions. added Avatar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -3,12 +3,18 @@
 import useRoutes from '@/app/hooks/useRoutes';
 import React, { useState } from 'react';
 import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
+import Avatar from '../Avatar';
 
-type DesktopSidebarProps = {};
+type DesktopSidebarProps = {
+  currentUser: User;
+};
 
-const DesktopSidebar: React.FC<DesktopSidebarProps> = () => {
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
-  const [isOpen, setisOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(currentUser);
   return (
     <div
       className="
@@ -40,6 +46,26 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav
+        className="
+        mt-4
+        flex
+        flex-col
+        justify-between
+        items-center
+      "
+      >
+        <div
+          onClick={() => setIsOpen(true)}
+          className="
+        cursor-pointer
+        hover:opacity-75
+        trasition
+        "
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
