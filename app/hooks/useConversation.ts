4a9949ada8a65e916c712cefeb38b41a470775bc
Matthew Fogarty <mattempyre@gmail.com>
2023-08-03 10:54:26 +0200

    Adding additonal hooks and auth for pusher

--- app/hooks/useConversation.ts
+++ app/hooks/useConversation.ts
@@ -3,16 +3,24 @@ import { useMemo } from 'react';
 
 const useConversation = () => {
   const params = useParams();
+
   const conversationId = useMemo(() => {
     if (!params?.conversationId) {
       return '';
     }
+
     return params.conversationId as string;
   }, [params?.conversationId]);
 
   const isOpen = useMemo(() => !!conversationId, [conversationId]);
 
-  return useMemo(() => ({ conversationId, isOpen }), [conversationId, isOpen]);
+  return useMemo(
+    () => ({
+      isOpen,
+      conversationId,
+    }),
+    [isOpen, conversationId]
+  );
 };
 
 export default useConversation;
